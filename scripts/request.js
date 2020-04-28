716fa35735fb0db73ba640b8dea88582b9bf40cb
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-04-28 16:07:14 -0700

    Added request_confirm.html, request.js, request_confirm.js, added the funcionality of making a request post and confirming it / going back to edit, local storage works

---
+++ scripts/request.js
@@ -0,0 +1,23 @@
+if (localStorage.getItem("subject") != null){
+    $("#subject").val(localStorage.getItem("subject"));
+}
+
+if (localStorage.getItem("grade") != null){
+    $("#grade").val(localStorage.getItem("grade"));
+}
+
+if (localStorage.getItem("details") != null){
+    $("#details").val(localStorage.getItem("details"));
+}
+
+
+
+
+$("#submit").click(function(){
+    localStorage.setItem("subject", $("#subject").val());
+    localStorage.setItem("grade", $("#grade").val());
+    localStorage.setItem("details", $("#details").val());
+    window.location.href="request_confirm.html";
+});
+
+

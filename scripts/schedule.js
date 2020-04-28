89b95024000819d8ab1ec221f79e10c54144c94e
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-04-28 15:17:26 -0700

    Changed date picker, formatted the page, changed time picker

--- scripts/schedule.js
+++ scripts/schedule.js
@@ -1,16 +1,28 @@
-$( function() {
-    $( "#datepicker" ).datepicker();
-  } );
+
+
+let d = new Date();
+let month = d.getMonth() + 1 + "";
+let day = d.getDate();
+let year = d.getFullYear();
+
+if (month.length == 1){
+    month = "0" + month;
+}
+
+document.querySelector("#date").value = year + "-" + month + "-" + day;
+
+
+
 $(function(){
     var $select = $(".hour");
-    for (i=0;i<=23;i++){
+    for (i=0;i<=11;i++){
         $select.append($('<option></option>').val(i).html(i))
     }
 });
 
 $(function(){
     var $select = $(".minute");
-    for (i=0;i<=59;i++){
+    for (i=0;i<=55;i = i + 5){
         $select.append($('<option></option>').val(i).html(i))
     }
 });

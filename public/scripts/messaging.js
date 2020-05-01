9c2babdf16d8d6036a78a19cbf74ffeeee57becf
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-04-30 20:41:28 -0700

    added messaging function

---
+++ public/scripts/messaging.js
@@ -0,0 +1,28 @@
+$("#bot").css({
+    "display": "block",
+    "width": "100%",
+    "position": "absolute",
+    "bottom": "0"
+})
+$("#sch").css({
+    "display":"block"
+})
+
+$("#textbox").css({
+    "width":"80%"
+})
+
+$("#send").css({
+    "width": "15%",
+    "position": "absolute",
+    "right": "0"
+})
+firebase.auth().onAuthStateChanged(function (user) {
+$("#send").click(function(){
+    console.log("1");
+    let str = document.querySelector("#textbox").value;
+    document.getElementById("chat").innerHTML += user.displayName + ": " + str + "<br>"
+    document.querySelector("#textbox").value = ""
+
+})
+})

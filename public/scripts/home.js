79d7d357348201932b444438fed50e4a53d718d3
Christina Raganit <craganit@my.bcit.ca>
2020-04-30 18:19:56 -0700

    home page

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,3 +1,11 @@
-firebase.auth().onAuthStateChanged(function (user) {
-    document.getElementById("username").innerHTML = user.displayName;
-});
+function showName() {
+    firebase.auth().onAuthStateChanged(function(user) {
+        document.getElementById("username").innerHTML = user.displayName;
+    });
+}
+
+function showMeetings() {
+    firebase.auth().onAuthStateChanged(function(user) {
+
+    });
+}

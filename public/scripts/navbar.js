a06f5f27bda3a990e1050a939ac6c1c52fc6c6ff
Dylan Sung <dys907@gmail.com>
2020-04-30 18:13:08 -0700

    Shop adds and reads currency from users account. Nav bar updated to also display current currency

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,8 +1,15 @@
-userInfo
+userInfo();
+
+
 
 function userInfo() {
     firebase.auth().onAuthStateChanged(function (user) {
-       let dbref = db.collection("users/").doc(user.uid);
+        let dbref = db.collection("users/").doc(user.uid);
        document.getElementById("dropdownId").innerHTML = user.displayName;
+       dbref.get()
+           .then(snap => {
+               let credits = snap.data().currency;
+               document.getElementById("current-cash").innerHTML = "Credits: " + credits; 
+           })
     });
 }

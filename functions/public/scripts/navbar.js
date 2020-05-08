57f9e45490da154f27b7a79a10c5248237fea928
Dylan Sung <dys907@gmail.com>
2020-05-07 18:24:02 -0700

    refreshes on database update

--- functions/public/scripts/navbar.js
+++ functions/public/scripts/navbar.js
@@ -1,18 +1,19 @@
-setInterval(userInfo,1000);
+userInfo();
 
 
 
 function userInfo() {
     firebase.auth().onAuthStateChanged(function (user) {
         let dbref = db.collection("users/").doc(user.uid);
-       document.getElementById("dropdownId").innerHTML = user.displayName;
-       dbref.get()
-           .then(snap => {
-               let credits = snap.data().currency;
-               $("#current-cash").text("Credits: " + credits);
-               
+        document.getElementById("dropdownId").innerHTML = user.displayName;
+        dbref.get()
+            .then(snap => {
+                dbref.onSnapshot(function (doc) {
+                    let credits = doc.data().currency;
+                    $("#current-cash").text("Credits: " + credits);
 
+                })
 
-           })
+            })
     });
 }

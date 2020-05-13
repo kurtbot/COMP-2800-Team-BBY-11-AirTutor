eb7aecc10d290415c26de2140939ca636ab9c0d7
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-05-13 13:38:40 -0700

    livechat works now, delete from database after sent and read, added livechat to navbar, a lot of cssing on livechat and messaging, fixed blocking issues on those pages

--- public/scripts/livechat.js
+++ public/scripts/livechat.js
@@ -1,47 +1,82 @@
-
-
-
+let currentTime = new Date();
 
 function loadRecentMessages() {
-    let msgOrder = db.collection("chatrooms/").doc(roomID).collection("messages");
-    // msgOrder.orderBy("actualTime").get().then(function (snap) {
-    //     snap.forEach(function (doc) {
-    //         $(".chat-log").html(
-    //             $(".chat-log").html() + doc.data().senderName + ": " + doc.data().message + "<br>"
-    //         );
-    //     })
-    // });
+    let msgOrder = db.collection("livechat/");
+
     msgOrder.orderBy("actualTime").onSnapshot(function(snapshot) {
         snapshot.docChanges().forEach(function(change) {
             if (change.type === "added") {
-                console.log("New city: ", change.doc.data());
-                $(".chat-log").html(
-                    $(".chat-log").html() + change.doc.data().senderName + ": " + change.doc.data().message + "<br>"
-                );
-                document.querySelector(".chat-window").scrollTop = document.querySelector(".chat-window").scrollHeight;
+                if (change.doc.data().actualTime >= currentTime.getTime()){
+                let promise = new Promise ((res, rej)=>{
+                    let newcontent1 = change.doc.data().senderName
+                    let newcontent2 = change.doc.data().timestamp
+                    let newcontent3 = change.doc.data().message
+                    let outerbox = $("<div></div>")
+                    $(outerbox).css({
+                        "display":"flex"
+                    })
+                    let newbox = $("<div></div>")
+                    $(newbox).html("<b>" + newcontent1 + "</b>" + " " + newcontent2 + "<br>" + newcontent3)
+                    $(newbox).css({
+                        "display":"inline-block"
+                    })
+                    console.log(newbox)
+                    if (firebase.auth().currentUser.uid == change.doc.data().senderID){
+                        $(newbox).css({
+                            "background-color":"rgb(69, 223, 110)",
+                            "border":" rgb(69, 223, 110) solid 1px",
+                            "border-radius":"10px",
+                            "margin":"3px",
+                            "padding":"5px",
+                            "color":"black"
+                        })
+                        $(outerbox).css({
+                            "justify-content":"flex-end"
+                        })
+                    } else {
+                        $(newbox).css({
+                            "background-color":"rgb(237 237 237)",
+                            "border":"rgb(237 237 237) solid 1px",
+                            "border-radius":"10px",
+                            "margin":"5px",
+                            "padding":"5px",
+                            "color":"black"
+                        })
+                        $(outerbox).css({
+                            "justify-content":"flex-start"
+                        })
+                    }
+
+                    // $(".chat-log").html(
+                    //     $(".chat-log").html() + newbox
+                    //     );
+                    $(outerbox).append($(newbox))
+                    $(".chat-log").append($(outerbox))
+                        res("success")
+                })
+                promise.then(function(){
+                    setTimeout(function(){
+                        db.collection("livechat").doc(change.doc.id).delete()
+                    }, 5000)
+
+                })
+                   
+                
+                window.scrollTo(0,document.body.scrollHeight);
+                }
             }
 
-            if (change.type === "removed") {
-                console.log("Removed city: ", change.doc.data());
-            }
         })});
 }
 
 firebase.auth().onAuthStateChanged(function (user) {
-
-    // Create the chat room
-    initializeRoom(user);
-
-    // Check if users are allowed to create a schedule
-    schedulingAllowed(user);
 
     // Load recent messages
     loadRecentMessages();
 
     // Add button event
     $(".send-btn").click(function () {
-        //localStorage.removeItem("roomID");
-        console.log(roomID);
+
         let d = new Date();
         let year = d.getFullYear();
         let month = d.getMonth() + 1;
@@ -53,26 +88,19 @@ firebase.auth().onAuthStateChanged(function (user) {
         // check input
         let str = document.querySelector(".chat-input").value;
         let output = document.querySelector(".chat-log");
-        // udpate chat log
-        // $(".chat-log").html(
-        //     $(".chat-log").html() + user.displayName + ": " + str + "<br>"
-        // );
 
         // remove input text
         $('.chat-input').val('');
 
         // add to database the sent chat
-        db.collection('chatrooms').doc(roomID).collection("messages").add({
+        db.collection('livechat').add({
             message: str,
             senderID: firebase.auth().currentUser.uid,
             senderName: user.displayName,
             timestamp: date,
-            actualTime: d
+            actualTime: d.getTime()
         });
-        db.collection("chatrooms").doc(roomID).update({
-            latest: d,
-            latestTime: date
-        })
+
 
     });
 
@@ -82,39 +110,29 @@ firebase.auth().onAuthStateChanged(function (user) {
 $(".chat-input").on('keyup', function (e) {
     if (e.keyCode === 13) {
         console.log("ahhhhh")
-                //localStorage.removeItem("roomID");
-                console.log(roomID);
-                let d = new Date();
-                let year = d.getFullYear();
-                let month = d.getMonth() + 1;
-                let day = d.getDate();
-                let hour = d.getHours();
-                let minute = d.getMinutes();
-                let date = year + "-" + month + "-" + day + " " + hour + ":" + minute;
-        
-                // check input
-                let str = document.querySelector(".chat-input").value;
-                let output = document.querySelector(".chat-log");
-                // udpate chat log
-                // $(".chat-log").html(
-                //     $(".chat-log").html() + user.displayName + ": " + str + "<br>"
-                // );
-        
-                // remove input text
-                $('.chat-input').val('');
-        
-                // add to database the sent chat
-                db.collection('chatrooms').doc(roomID).collection("messages").add({
-                    message: str,
-                    senderID: firebase.auth().currentUser.uid,
-                    senderName: user.displayName,
-                    timestamp: date,
-                    actualTime: d
-                });
-                db.collection("chatrooms").doc(roomID).update({
-                    latest: d,
-                    latestTime: date
-                })
+        let d = new Date();
+        let year = d.getFullYear();
+        let month = d.getMonth() + 1;
+        let day = d.getDate();
+        let hour = d.getHours();
+        let minute = d.getMinutes();
+        let date = year + "-" + month + "-" + day + " " + hour + ":" + minute;
+
+        // check input
+        let str = document.querySelector(".chat-input").value;
+        let output = document.querySelector(".chat-log");
+
+        // remove input text
+        $('.chat-input').val('');
+
+        // add to database the sent chat
+        db.collection('livechat').add({
+            message: str,
+            senderID: firebase.auth().currentUser.uid,
+            senderName: user.displayName,
+            timestamp: date,
+            actualTime: d.getTime()
+        });
     }
 });
 })

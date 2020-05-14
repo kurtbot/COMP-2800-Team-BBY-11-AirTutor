9c26ab29c85f3c8d5d159dc460f140c6008b87ae
Dylan Sung <dys907@gmail.com>
2020-05-14 13:50:00 -0700

    live updates on nav bar now

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,6 +1,6 @@
 userInfo();
 loggedIn();
-
+updateMessage();
 
 function userInfo() {
     firebase.auth().onAuthStateChanged(function (user) {
@@ -30,5 +30,33 @@ function loggedIn() {
             $("#hideme").show();
             $("#hideme").css('display', 'flex')
         }
+    })
+}
+
+function updateMessage() {
+    firebase.auth().onAuthStateChanged(function (user) {
+        let dbref= db.collection('/chatrooms/');
+        dbref.onSnapshot(function(snapshot){
+            snapshot.docChanges().forEach(function (change) {
+                if(change.type === "added" || change.type === "modified" ) {
+
+                    if (change.doc.data().studentid == user.uid ) {
+                        if(change.doc.data().unreadstudent == true) {
+                            $("#new-message").text("Message*").css("color","red");
+                        } else {
+                            $("#new-message").text("Message").css("color","grey");
+                        }
+                    }
+                    
+                    if (change.doc.data().tutorid == user.uid) {
+                        if(change.doc.data().unreadtutor == true) {
+                            $("#new-message").text("Message*").css("color","red");
+                        } else {
+                            $("#new-message").text("Message").css("color","grey");
+                        }
+                    }
+                }
+            })
+        })
     })
 }

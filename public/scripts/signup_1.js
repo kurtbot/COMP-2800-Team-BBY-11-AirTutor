ea20e37253e805a9351568f8d7ef1734d3389f2e
kurtbot <56173650+kurtbot@users.noreply.github.com>
2020-05-15 13:21:28 -0700

    fixed google sign in

--- public/scripts/signup_1.js
+++ public/scripts/signup_1.js
@@ -3,6 +3,18 @@ $('.signup-button').click(signInWithGoogle);
 function signInWithGoogle() {
     firebase.auth().signInWithPopup(new firebase.auth.GoogleAuthProvider())
         .then(function () {
-            window.location.href = '/signup'
-        })
+
+            var docRef = db.collection("users").doc(firebase.auth().currentUser.uid);
+
+            docRef.get().then(function (doc) {
+                if (doc.exists) {
+                    window.location.href = '/home'
+                } else {
+                    window.location.href = '/signup'
+                }
+            }).catch(function (error) {
+                console.log("Error getting document:", error);
+            });
+
+        });
 }

1aac25d3983e70d0394018e7fd7f1a8082511043
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-05-18 23:18:49 -0700

    cleaned up messages.js

--- public/scripts/messages.js
+++ public/scripts/messages.js
@@ -1,142 +1,29 @@
-// Display all existing messages when document is ready
+// Display all existing chatrooms when document is ready
 $(document).ready(loadMessages);
 /**
- * Create the messages.
- * Read through the database for messages, and add them in divs to be displayed.
+ * Read through the database for chatrooms.
  */
 function loadMessages(){
     let chat = db.collection("chatrooms/");
     chat.orderBy("latest").onSnapshot(function (snapshot) {
         snapshot.docChanges().forEach(function (change) {
+            // Add the chatroom when it's created or modified
             if (change.type === "added" || change.type === "modified") {
-                let n1 = change.doc.data().tutorid;
-                let n2 = change.doc.data().studentid;
-                let container = $("<div></div>").attr("id", change.doc.id)
-                let inner = $("<div></div>").css({
-                    display: "inline"
-                })
-                $(container).css({
-                    "border": "#EEEEEE solid 1px",
-                    "padding": "5px"
-                }).hover(
-                    function () {
-                        $(this).addClass("hover");
-                    },
-                    function () {
-                        $(this).removeClass("hover");
-                    }
-                )
-                    
-                let name = $("<b></b>").css({
-                    "display": "inline-block",
-                    "width": "20%",
-                    "margin-left": "3%",
-                })
-                let topic = $("<span></span>").html("Topic: " + change.doc.data().topic).css({
-                    "display": "inline-block",
-                    "width": "40%"
-                })
-                let msgtime = $("<span></span>").html(change.doc.data().latestTime).css({
-                    "display": "inline-block",
-                    "width": "30%"
-                })
-                if (change.doc.data().tutorid == firebase.auth().currentUser.uid) {
-                    let docRef = db.collection("users/").doc(n2);
-                    docRef.get().then(function (docc) {
-                    name.innerHTML =
-                        "User: " + docc.data().firstName + " " + docc.data().lastName;
-                    });
-                } else {
-                    let docRef = db.collection("users/").doc(n1);
-                    docRef.get().then(function (docc) {
-                    name.innerHTML =
-                        "User: " + docc.data().firstName + " " + docc.data().lastName;
-                    });
-                }
-            
-                let del = $("<input type=image>");
-            
-                $(del).click(function () {
-                    if (
-                    confirm(
-                        "Are you sure you want to delete this chat and all its history? (It will be deleted for the other user too)"
-                    )
-                    ) {
-                    db.collection("chatrooms")
-                        .doc(change.doc.id)
-                        .collection("messages")
-                        .get()
-                        .then(function (snap) {
-                        snap.forEach(function (docc) {
-                            db.collection("chatrooms")
-                            .doc(change.doc.id)
-                            .collection("messages")
-                            .doc(docc.id)
-                            .delete();
-                        });
-                        });
-                    db.collection("chatrooms").doc(change.doc.id).delete();
-                    }
-                });
-        
-                $(del).attr("src", "./src/no.png");
-            
-                $(del).css({
-                    position: "absolute",
-            
-                    right: "20px",
-                    border: "none",
-                    width: "20px",
-                    height: "20px",
-                });
-            
-                $(inner).click(function () {
-                    localStorage.setItem("roomID", change.doc.id);
-                    localStorage.setItem("requestID", change.doc.data().requestid);
-                    window.location.href = "/messaging";
-                });
-                $(inner).append(name, topic, msgtime)
-                $(container).append(inner, del)
-
-                if (
-                    change.doc.data().studentid == firebase.auth().currentUser.uid ||
-                    change.doc.data().tutorid == firebase.auth().currentUser.uid
-                ) {
-                    if (!document.getElementById(change.doc.id)) {
-                        if (firebase.auth().currentUser.uid == change.doc.data().studentid) {
-                            if (change.doc.data().unreadstudent) {
-                                $(container).css({
-                                    "background-color": "lightblue",
-                                });
-                            }
-                        } else {
-                            if (change.doc.data().unreadtutor) {
-                                $(container).css({
-                                "background-color": "lightblue",
-                                });
-                            }
-                        }
-                    $("#msgs").prepend(container);
-                    } else {
-                        if (firebase.auth().currentUser.uid == change.doc.data().studentid) {
-                            if (change.doc.data().unreadstudent) {
-                                $(container).css({
-                                    "background-color": "lightblue",
-                                });
-                            }
-                        } else {
-                            if (change.doc.data().unreadtutor) {
-                                $(container).css({
-                                    "background-color": "lightblue",
-                                });
-                            }
-                        }
-                        $("#" + change.doc.id).remove();
-            
-                        $("#msgs").prepend(container);
-                    }
-                }
+                // Read and save required data from the database
+                let roomID = change.doc.id;
+                let tutorid = change.doc.data().tutorid;
+                let studentid = change.doc.data().studentid;
+                let tutor = change.doc.data().tutorname;
+                let student = change.doc.data().studentname;
+                let unreadtutor = change.doc.data().unreadtutor;
+                let unreadstudent = change.doc.data().unreadstudent;
+                let topicstr = change.doc.data().topic;
+                let latestTime = change.doc.data().latestTime;
+                let requestid = change.doc.data().requestid;
+  
+                createMessage(roomID, tutorid, studentid, tutor, student, unreadtutor, unreadstudent, topicstr, latestTime, requestid)
             }
+            // Remove the chatroom if it's deleted from the database
             if (change.type === "removed") {
                 $("#" + change.doc.id).remove();
             }
@@ -144,5 +31,116 @@ function loadMessages(){
     });
     
 }
+/**
+ * Create the elements for this chatroom div and add them into the div.
+ * @param {*} roomID the chatroom's doc id
+ * @param {*} tutorid this chatroom's tutor's user id
+ * @param {*} studentid this chatroom's student's user id
+ * @param {*} tutor this chatroom's tutor name
+ * @param {*} student this chatroom's student name
+ * @param {*} unreadtutor if the tutor has not read the latest message
+ * @param {*} unreadstudent if the student has not read the latest message
+ * @param {*} topicstr topic of the post that led to this chat
+ * @param {*} latestTime sent time of the latest message in this room
+ * @param {*} requestid doc id of the post that led to this chat
+ */
+function createMessage(roomID, tutorid, studentid, tutor, student, unreadtutor, unreadstudent, topicstr, latestTime, requestid){    
+    let str = tutorid == firebase.auth().currentUser.uid? student : tutor;
+    let name = $("<b></b>").html("User: " + str).css({
+        "display": "inline-block",
+        "width": "20%",
+        "margin-left": "3%",
+    })
+    let topic = $("<span></span>").html("Topic: " + topicstr).css({
+        "display": "inline-block",
+        "width": "40%"
+    })
+    let msgtime = $("<span></span>").html(latestTime).css({
+        "display": "inline-block",
+        "width": "30%"
+    })
+    let del = $("<input type=image>").click(function () {deleteChat(roomID)}).attr("src", "./src/no.png").css({
+        "position": "absolute",
+        "right": "20px",
+        "width": "20px",
+        "height": "20px"
+    });
+    let inner = $("<div></div>").css({
+        "display": "inline"
+    }).click(function () {
+        localStorage.setItem("roomID", roomID);
+        localStorage.setItem("requestID", requestid);
+        window.location.href = "/messaging";
+    }).append(name, topic, msgtime)
+    let container = $("<div></div>").attr("id", roomID).css({
+        "border": "#EEEEEE solid 1px",
+        "padding": "5px"
+    }).hover(
+        function () {
+            $(this).addClass("hover");
+        },
+        function () {
+            $(this).removeClass("hover");
+        }
+    ).append(inner, del)
+    includeMessage(studentid, tutorid, roomID, unreadstudent, unreadtutor, container)
+}
 
+/**
+ * Delete this chatroom.
+ * @param {*} roomID the chatroom's id
+ */
+function deleteChat(roomID){
+    if (
+        confirm(
+            "Are you sure you want to delete this chat and all its history? (It will be deleted for the other user too)"
+        )
+        ) {
+            db.collection("chatrooms").doc(roomID).collection("messages").get().then(function (snap) {
+                snap.forEach(function (docc) {
+                    db.collection("chatrooms").doc(roomID).collection("messages").doc(docc.id).delete();
+                });
+            });
+            db.collection("chatrooms").doc(roomID).delete();
+        }
+}
 
+/**
+ * Add this chatroom div onto the page if the user is one of the chatroom participants
+ * @param {*} studentid the chatroom's student's user id
+ * @param {*} tutorid the chatroom's tutor's user id
+ * @param {*} roomID the chatroom's id
+ * @param {*} unreadstudent if the student has not read the latest message
+ * @param {*} unreadtutor if the tutor has not read the latest message
+ * @param {*} container div containing this chatroom on the page
+ */
+function includeMessage(studentid, tutorid, roomID, unreadstudent, unreadtutor, container){
+    let user = firebase.auth().currentUser.uid;
+    // Check if this user is one of the chatroom's participants
+    if (studentid == user || tutorid == user) {
+        if (firebase.auth().currentUser.uid == studentid) {
+            if (unreadstudent) {
+                unreadMessage(container)
+            }
+        } else {
+            if (unreadtutor) {
+                unreadMessage(container)
+            }
+        }
+        // If this chat already exists on the page, remove it (so it can be added on top after)
+        if ($("#" + roomID)){
+            $("#" + roomID).remove();
+        }
+        $("#msgs").prepend(container);
+    }
+}
+
+/**
+ * CSS the chatroom if it has unread messages.
+ * @param {*} container 
+ */
+function unreadMessage(container){
+    $(container).css({
+        "background-color": "lightblue",
+    });
+}

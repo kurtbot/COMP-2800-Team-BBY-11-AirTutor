5b742650ca82df9c2e673ed880f67c174c9d3566
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-05-19 14:00:38 -0700

    made the delete part into a function, javadoc'ed vicly's part

--- public/scripts/rating.js
+++ public/scripts/rating.js
@@ -39,37 +39,26 @@ function submitYesReview() {
 async function submitNoReview() {
     let promise = new Promise((res, rej)=>{
         let dbref2 = db.collection("users/").doc(queryResult());
-        // let up = firebase.firestore.FieldValue.increment(1)
         let reason = $("#complaint").val()
         dbref2.set({
             flagged: true,
             reason: reason
         }, {merge: true})
         
-        db.collection("sessionrooms").doc(localStorage.getItem("session")).delete().then(function(){
-            db.collection("schedules").doc(localStorage.getItem("schedule")).delete().then(function() {
-                db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages")
-                .get().then(function(snap){
-                    snap.forEach(function(docc){
-                        db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages").doc(docc.id).delete()
-                    })
-
-                }).then(function(){
-                    db.collection("chatrooms").doc(localStorage.getItem("chat")).delete().then(function(){
-                        res("success")
-                    })
-                })
-
-          })
+        deleteInfo(false).then(function() {
+            res('success');
         })
-        // db.collection("sessionrooms").doc(localStorage.getItem("session")).delete()
-        // db.collection("schedules").doc(localStorage.getItem("schedule")).delete()
+        
         console.log("step 1")
 
-    }
-    )
-
+    })
+    // Remove local storage data and go to home page
     promise.then(result =>{
+        localStorage.removeItem("creditxfer")
+        localStorage.removeItem("request")
+        localStorage.removeItem("session")
+        localStorage.removeItem("schedule")
+        localStorage.removeItem("chat")
         window.location.href = "/home"
         console.log("step 2")
     })
@@ -82,59 +71,46 @@ function reviewSubmit() {
         let promise = new Promise((res, rej) =>{
             
         
-        let amount = parseInt(localStorage.getItem("creditxfer"));
-        let minus = amount * (-1);
-        let decrement = firebase.firestore.FieldValue.increment(minus);
-        let increment = firebase.firestore.FieldValue.increment(amount);
-        let up = firebase.firestore.FieldValue.increment(1)
-        let dbref = db.collection("users/").doc(user.uid);
-        dbref.update({
-            currency: decrement
-        })
-        let dbref2 = db.collection("users/").doc(queryResult());
+            let amount = parseInt(localStorage.getItem("creditxfer"));
+            let minus = amount * (-1);
+            let decrement = firebase.firestore.FieldValue.increment(minus);
+            let increment = firebase.firestore.FieldValue.increment(amount);
+            let up = firebase.firestore.FieldValue.increment(1)
+            let dbref = db.collection("users/").doc(user.uid);
+            dbref.update({
+                currency: decrement
+            })
+            let dbref2 = db.collection("users/").doc(queryResult());
 
-        dbref2.set({
-            currency: increment,
-            answeredQuestion: up
-        }, {merge: true})
-        let request = localStorage.getItem("request");
-        console.log(request)
-        let professional = document.getElementById("pro").value;
-        let teaching = document.getElementById("teaching-qual").value;
-        let reviewedAccount = db.collection("users/").doc(queryResult());
-    
-        reviewedAccount.collection("review").add({
-            professionalism: parseInt(professional) + 1,
-            teachingquality: parseInt(teaching) + 1
-        })
-        db.collection("posts").doc(request).delete().then(function(){
-        db.collection("sessionrooms").doc(localStorage.getItem("session")).delete().then(function(){
-            db.collection("schedules").doc(localStorage.getItem("schedule")).delete().then(function() {
-                db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages")
-                .get().then(function(snap){
-                    snap.forEach(function(docc){
-                        db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages").doc(docc.id).delete()
-                    })
-
-                }).then(function(){
-                    if ($("#deletechat").val() == "Yes"){
-                    db.collection("chatrooms").doc(localStorage.getItem("chat")).delete().then(function(){
-                        res("success")
-                    })
-                } else {
-                    res("success")
-                }
+            dbref2.set({
+                currency: increment,
+                answeredQuestion: up
+            }, {merge: true})
+            let request = localStorage.getItem("request");
+            console.log(request)
+            let professional = document.getElementById("pro").value;
+            let teaching = document.getElementById("teaching-qual").value;
+            let reviewedAccount = db.collection("users/").doc(queryResult());
+        
+            reviewedAccount.collection("review").add({
+                professionalism: parseInt(professional) + 1,
+                teachingquality: parseInt(teaching) + 1
+            })
+            // Delete the request leading to this session
+            db.collection("posts").doc(request).delete().then(function(){
+                deleteInfo(true).then(function() {
+                    res('success');
                 })
+            })
 
-          })
         })
-    })
-
-    })
-
+        // Remove local storage data and go to home page
         promise.then(function(){
             localStorage.removeItem("creditxfer")
             localStorage.removeItem("request")
+            localStorage.removeItem("session")
+            localStorage.removeItem("schedule")
+            localStorage.removeItem("chat")
             window.location.href="/home";
         })
         
@@ -143,4 +119,38 @@ function reviewSubmit() {
 
 
 
+}
+
+/**
+ * Delete the session, schedule, and chat history between these two users for this meeting.
+ * Delete chatroom if user answered "no" to if tutor answered the question, or if user chose to delete chatroom.
+ * @param {*} review boolean answer to if the tutor answered the question
+ */
+function deleteInfo (review) {
+    let promise = new Promise((res, rej) => {
+        db.collection("sessionrooms").doc(localStorage.getItem("session")).delete().then(function(){
+            db.collection("schedules").doc(localStorage.getItem("schedule")).delete().then(function() {
+                db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages")
+                .get().then(function(snap){
+                    // Delete the subcollection messages under chatroom
+                    snap.forEach(function(docc){
+                        db.collection("chatrooms").doc(localStorage.getItem("chat")).collection("messages").doc(docc.id).delete()
+                    })
+
+                }).then(function(){
+                    // Delete chatroom if user chose to or user said tutor did not answer the question
+                    if (!review || $("#deletechat").val() == "Yes"){
+                        db.collection("chatrooms").doc(localStorage.getItem("chat")).delete()  
+                    } else {
+                        console.log("ok")
+                    }
+                }).then(function(){
+                    res("success")
+                })
+                
+            })
+        })
+    })
+
+    return promise;
 }

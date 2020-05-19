54008e87255eee4e8595d638e48a781b674d53ae
Vicly Xinyue Cao <viclycao1211@gmail.com>
2020-05-19 00:39:34 -0700

    cleaned up request and schedule js'es, missing field now turns red

--- public/scripts/request.js
+++ public/scripts/request.js
@@ -1,3 +1,4 @@
+// If local storage already have the following info, display them
 if (localStorage.getItem("subject") != null){
     $("#subject").val(localStorage.getItem("subject"));
 }
@@ -14,25 +15,57 @@ if (localStorage.getItem("retitle") != null){
     $("#retitle").val(localStorage.getItem("retitle"));
 }
 
+/**
+ * Clicking cancel leaves the request page and goes back to home page
+ */
 $("#cancel").click(function(){
     window.location.href = "/home";
 })
 
-
+/**
+ * Clicking submit leads to a confirmation page, where the entered info is displayed
+ */
 $("#submit").click(function(){
+    // Check if title and details (required fields) are filled
     let condition1 = $("#retitle").val().trim();
     let condition2 = $("#details").val().trim();
     if (condition1 && condition2){
+        // Get these values from the input fields and save them into local storage to be displayed on confirmation page
         localStorage.setItem("subject", $("#subject").val());
         localStorage.setItem("grade", $("#grade").val());
         localStorage.setItem("details", $("#details").val());
         localStorage.setItem("retitle", $("#retitle").val());
+        // Go to confirmation page
         window.location.href="/request_confirm";
+    // Alert user to fill the required fields if they are not filled
     } else {
+        showMissing(condition1, condition2)
         alert("Please fill out the required fields")
     }
-
-
 });
 
-
+/**
+ * Turn the missing required field red.
+ * @param {*} condition1 if the title field is filled
+ * @param {*} condition2 if the details field is filled
+ */
+function showMissing(condition1, condition2){
+    if (!condition1){
+        $("#require1").css({
+            "color":"red"
+        })
+    } else {
+        $("#require1").css({
+            "color":"black"
+        })
+    }
+    if (!condition2){
+        $("#require2").css({
+            "color":"red"
+        })
+    } else {
+        $("#require2").css({
+            "color":"black"
+        })
+    }
+}

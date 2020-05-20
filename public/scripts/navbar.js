de88c72107fc55b29946e42813a6956812638312
Dylan Sung <dys907@gmail.com>
2020-05-19 17:35:04 -0700

    deleted extra link on navbar.js

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -83,11 +83,9 @@ function understood() {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
     document.getElementById("main-sidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("main-sidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
 }

2616524eb291956cc3d3d061fe2f202dad57ff71
Dylan Sung <dys907@gmail.com>
2020-05-19 19:54:24 -0700

    Responsive issue on profile and view profile fixed. Edit profile code is cleaner

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -81,12 +81,12 @@ function understood() {
     $("#warning").fadeOut("slow");
 }
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+/* Set the width of the side navigation to 250px */
 function openNav() {
     document.getElementById("main-sidenav").style.width = "250px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+/* Set the width of the side navigation to 0*/
 function closeNav() {
     document.getElementById("main-sidenav").style.width = "0";
 }
